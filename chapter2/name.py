d2c85e0aff32dad43d48fc741e4a72d6edc57057
wangchunsheng24 <wangchunsheng24@jd.com>
2026-02-10 11:26:08 +0800

    add code

---
+++ chapter2/name.py
@@ -0,0 +1,17 @@
+first_name = "yashu"
+last_name = 'ning'
+full_name = first_name + " " + last_name
+print("hello," +  full_name.title() + "!")
+print("hello," +  full_name.upper() + "!")
+print("hello," +  full_name.lower() + "!")
+
+favorite_lang = "python "
+print(favorite_lang.rstrip())
+# it's a girl
+favorite_girl = " yashu"
+print(favorite_girl.lstrip().title())
+print(favorite_girl.strip().title())
+
+print("i love your " + str(30))
+
+print(3 + 5)
---
+++ chapter3/list.py
@@ -0,0 +1,6 @@
+motorcycles = ["abc","def"]
+print(motorcycles)
+motorcycles.append("cds")
+print(motorcycles)
+motorcycles.insert(0, "ooo")
+print(motorcycles)
